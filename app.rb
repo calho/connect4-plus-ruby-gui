1cb5fdc4cd51fa420f43801a0d83637d6ed6838f
rmaarouf <rmaarouf@ualberta.ca>
2017-03-24 15:27:17 -0600

    just more stuff

--- app.rb
+++ app.rb
@@ -6,7 +6,14 @@ class App
 	def initialize
 		Gtk.init
         @builder = Gtk::Builder::new
-        @builder.add_from_file("window.glade")
+        @builder.add_from_file("window2.glade")
+
+
+        window = @builder.get_object("window")
+        #window.signal_connect("destroy") { Gtk.main_quit }
+        window.show()
+        Gtk.main()
+
 	end
 
 
