9097a9fdd1dc70faba61595fe0d4fe06417d4dda
rmaarouf <rmaarouf@ualberta.ca>
2017-03-26 18:53:33 -0600

    just for aaron

--- AI.rb
+++ AI.rb
@@ -5,176 +5,72 @@ class AI
 
 	end
 
-	# def common_loop_operations(row_index,column_index,player_self,player_id,enemy_id,score_counter)
-
-	# 	proc = Proc.new()
-	# 	{
-	# 		if board_array[row_index][column_index] != 0
-	# 			found_piece = true
-	# 		end
-
-	# 		if player_id == board_array[row_index][column_index] && player_self
-	# 			score_counter=score_counter+1
-
-	# 		end
-
-	# 		if player_id == board_array[row_index][column_index] && !player_self
-	# 			score_counter = 0
-	# 			player_self = true
-
-	# 		end
-
-	# 		if enemy_id == board_array[row_index][column_index] && player_self
-	# 			score_counter = 0
-	# 			player_self = false
-
-	# 		end
-
-
-	# 		if enemy_id == board_array[row_index][column_index] && !player_self
-	# 			score_counter=score_counter+1
-	# 		end
-
-
-	# 		if board_array[row_index][column_index] == 0 && found_piece
-	# 			p board_array[row_index][column_index]
-	# 			id =(player_self)? player_id : enemy_id
-	# 			if score_counter == 0
-	# 				score_counter=1
-	# 			end
-	# 			score = score_counter*10 +((player_self)? 5 : 0)
-	# 			p id
-	# 			score_hash[[row_index,column_index]]=[id,score]
-	# 			found_piece = false
-	# 		end
-	# 	}
-
-	# end
-
-
-	def compute_horizontal_score(board_array,player,enemy,direction)
-
+	def compute_horizontal_score(board_array,player,direction)
 		player_id = player.get_id
-		enemy_id = enemy.get_id
+		
 		player_pattern = player.get_win_pattern
-		enemy_pattern = enemy.get_win_pattern
 
 		score_hash = Hash.new
 		board_array.each_with_index do |row , row_index|
 			score_counter = 0
-			# score_counter_enemy=0
-			player_self =true
 			found_piece = false
 			row=(direction)? row : row.reverse
-			p row
-			pattern_counter=0
 			row.each_with_index do |column, column_index|
 				c_index=(direction)? column_index : 6 - column_index
 
-				if board_array[row_index][c_index] != 0
+				if player_pattern[score_counter] == board_array[row_index][c_index]
+					score_counter=score_counter+1
 					found_piece = true
-				end
-
-				if player_pattern[score_counter] == board_array[row_index][c_index] && player_self
-					score_counter=score_counter+1
-
-				end
-
-				if player_pattern[score_counter] == board_array[row_index][c_index] && !player_self
-					score_counter = 0
-					player_self = true
-
-				end
-
-				if enemy_pattern[score_counter] == board_array[row_index][c_index] && player_self
-					score_counter = 0
-					player_self = false
-
-				end
-
-
-				if enemy_pattern[score_counter] == board_array[row_index][c_index] && !player_self
-					score_counter=score_counter+1
 				end
 
 				if board_array[row_index][c_index] == 0 && found_piece
-					p board_array[row_index][c_index]
-					id =(player_self)? player_id : enemy_id
+					# p board_array[row_index][c_index]
 					if score_counter == 0
 						score_counter=1
 					end
-					score = score_counter*10 +((player_self)? 5 : 0)
-					p id
-					score_hash[[row_index,c_index]]=[id,score]
+					score = score_counter*10 
+					score_hash[[row_index,c_index]]=[player_id,score]
 					found_piece = false
 				end
-
 			end
-
 		end
 		return score_hash
+
 	end
 
 
-
-
-
-	def compute_vertical_score(board_array, player_id, enemy_id)
-		# gravity makes this nice
+	def compute_vertical_score(board_array, player)
+		player_pattern = player.get_win_pattern
+		player_id = player.get_id
 		score_hash = Hash.new
-
 		for column_index in 0..board_array[0].length-1
-			
-			player_self =true
 			found_piece = false
 			score_counter = 0
 			board_array.each_with_index do |row , row_index|
-				p board_array[row_index][column_index]
-
-				if board_array[row_index][column_index] != 0
+				
+				if player_pattern[score_counter] == board_array[row_index][column_index]
+					score_counter=score_counter+1
 					found_piece = true
+				elsif board_array[row_index][column_index] != 0
+					found_piece = false
+					score_counter=0
 				end
-
-				if player_id == board_array[row_index][column_index] && player_self
-					score_counter=score_counter+1
-
-				end
-
-				if player_id == board_array[row_index][column_index] && !player_self
-					score_counter = 0
-					player_self = true
-
-				end
-
-				if enemy_id == board_array[row_index][column_index] && player_self
-					score_counter = 0
-					player_self = false
-
-				end
-
-
-				if enemy_id == board_array[row_index][column_index] && !player_self
-					score_counter=score_counter+1
-				end
-
 
 				if board_array[row_index][column_index] == 0 && found_piece
-					p board_array[row_index][column_index]
-					id =(player_self)? player_id : enemy_id
+					# p board_array[row_index][c_index]
 					if score_counter == 0
 						score_counter=1
 					end
-					score = score_counter*10 +((player_self)? 5 : 0)
-					p id
-					score_hash[[row_index,column_index]]=[id,score]
+					score = score_counter*10 
+					score_hash[[row_index,column_index]]=[player_id,score]
 					found_piece = false
 				end
-
 			end
 		end
-
 		return score_hash
 	end
+
+	# need to diagonal
 
 	def compute_right_diagonal_score(board_array,player_id,enemy_id)
 
--- test.rb
+++ test.rb
@@ -1,15 +1,88 @@
 
-
-
-
-
-
+require_relative 'Player'
 
 def compute_horizontal_score(board_array,player,direction)
+	player_id = player.get_id
+	
+	player_pattern = player.get_win_pattern
+
 	score_hash = Hash.new
 	board_array.each_with_index do |row , row_index|
 		score_counter = 0
 		found_piece = false
 		row=(direction)? row : row.reverse
-		
+		row.each_with_index do |column, column_index|
+			c_index=(direction)? column_index : 6 - column_index
+
+			if player_pattern[score_counter] == board_array[row_index][c_index]
+				score_counter=score_counter+1
+				found_piece = true
+			elsif board_array[row_index][column_index] != 0
+				found_piece = false
+				score_counter=0
+			end
+
+			if board_array[row_index][c_index] == 0 && found_piece
+				# p board_array[row_index][c_index]
+				if score_counter == 0
+					score_counter=1
+				end
+				score = score_counter*10 
+				score_hash[[row_index,c_index]]=[player_id,score]
+				found_piece = false
+			end
+		end
+	end
+	return score_hash
+
 end
+
+
+
+
+
+
+
+
+def compute_vertical_score(board_array, player)
+	player_pattern = player.get_win_pattern
+	player_id = player.get_id
+	score_hash = Hash.new
+	for column_index in 0..board_array[0].length-1
+		found_piece = false
+		score_counter = 0
+		board_array.each_with_index do |row , row_index|
+			
+			if player_pattern[score_counter] == board_array[row_index][column_index]
+				score_counter=score_counter+1
+				found_piece = true
+			elsif board_array[row_index][column_index] != 0
+				found_piece = false
+				score_counter=0
+			end
+
+			if board_array[row_index][column_index] == 0 && found_piece
+				# p board_array[row_index][c_index]
+				if score_counter == 0
+					score_counter=1
+				end
+				score = score_counter*10 
+				score_hash[[row_index,column_index]]=[player_id,score]
+				found_piece = false
+			end
+		end
+	end
+	return score_hash
+end
+
+
+board_array = Array.new(6){Array.new(7,0)}
+player1 = Player.new(1,"J",[1,1,1,1])
+
+board_array[0][0] = 1
+board_array[0][1] = 1
+board_array[1][0] = 2
+board_array[1][1] = 2
+board_array[2][0] = 1
+board_array[2][1] = 1
+p compute_vertical_score(board_array,player1)
