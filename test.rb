32f87e4f0ad64971dce0be9dc53e17d21ede18be
rmaarouf <rmaarouf@ualberta.ca>
2017-03-26 18:04:31 -0600

    just more stuff again moreeee morreeeeee moreeeee

--- test.rb
+++ test.rb
@@ -5,4 +5,11 @@
 
 
 
-def compute_
+def compute_horizontal_score(board_array,player,direction)
+	score_hash = Hash.new
+	board_array.each_with_index do |row , row_index|
+		score_counter = 0
+		found_piece = false
+		row=(direction)? row : row.reverse
+		
+end
