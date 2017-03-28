46fe24bd3a1cb699cb262ab959a83de400b0c9fd
calho <clho@ualberta.ca>
2017-03-28 14:35:10 -0600

    done maybe

--- contracts.rb
+++ contracts.rb
@@ -51,7 +51,6 @@ def invariant_Manager( board_model, player_list, last_player_id, ai, game_state)
 	assert_respond_to(board_model, :add_piece, 'invalid board model')
 	assert_respond_to(player_list, :at, 'invalid player_list')
 	assert(last_player_id >= 0, 'negative last player id')
-	# assert_respond_to(ai, :possible_moves, 'invalid ai')
 	assert((defined? game_state) != nil, 'game state not defined' )
 end
 
